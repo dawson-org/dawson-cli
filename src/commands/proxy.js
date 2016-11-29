8cfc0b306f26627b7e7c11dcb0609ff0ccf65300
Simone Lusenti <simone@plasticpanda.com>
2016-11-29 18:17:38 +0100

    do not report response size

--- src/commands/proxy.js
+++ src/commands/proxy.js
@@ -24,7 +24,7 @@ import AWS from 'aws-sdk';
 const sts = new AWS.STS({});
 const iam = new AWS.IAM({});
 
-import { debug, warning, error, success } from '../logger';
+import { log, debug, warning, error, success } from '../logger';
 import { SETTINGS, API_DEFINITIONS, APP_NAME } from '../config';
 import { compare } from '../libs/pathmatch';
 
@@ -145,29 +145,35 @@ async function processAPIRequest (req, res, { body, outputs, resources, pathname
       res.end();
       return;
     }
-    res.writeHead(200, { 'Content-Type': contentType });
-    if (!data) {
-      error(`Handler returned an empty body`);
-    } else {
-      if (contentType === 'application/json') {
-        res.write(data.response);
-      } else if (contentType === 'text/plain') {
-        res.write(data.response);
-      } else if (contentType === 'text/html') {
-        res.write(data.html);
-      } else {
-        res.write(data.response);
-      }
+    if (typeof data.response !== 'string' && !data.html ||
+        typeof data.html !== 'string' && !data.response ||
+        (typeof data.response === 'undefined' && typeof data.html === 'undefined')) {
+      error(`Your function must return a string, unless 'responseContentType' is application/json.`);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.write('dawson message: function returned an invalid body');
+      res.end();
+      return;
     }
-    console.log(` <- END '${runner.name}' (${new Intl.NumberFormat().format(data.length / 1024)} KB)\n`.red.dim);
+    res.writeHead(200, { 'Content-Type': contentType });
+    if (contentType === 'application/json') {
+      res.write(data.response);
+    } else if (contentType === 'text/plain') {
+      res.write(data.response);
+    } else if (contentType === 'text/html') {
+      res.write(data.html);
+    } else {
+      res.write(data.response);
+    }
+    log('End method execution');
     res.end();
+    return;
   };
 
-  console.log(`\n -> START '${runner.name}'`.green.dim);
   const credentials = await assumeRole(resources, runner);
 
   const doCall = () => {
     try {
+      console.log(`\nBegin method execution: '${runner.name}'`);
       const invokeResult = dockerLambda({
         event,
         taskDir: `${process.cwd()}/.dawson-dist`,
@@ -412,7 +418,6 @@ export function run (argv) {
     error('Server error', err);
   });
 
-  process.stdout.write('\x1B[2J\x1B[0f');
   taskCreateBundle({
     appStageName: stage,
     noUpload: true,
