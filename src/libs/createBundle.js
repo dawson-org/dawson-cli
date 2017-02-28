20d6d1e1aeced2f8dbf29f3756bd802d9f589786
Simone Lusenti <simone@plasticpanda.com>
2017-02-28 17:23:19 +0100

    updates babel command detection

--- src/libs/createBundle.js
+++ src/libs/createBundle.js
@@ -54,9 +54,24 @@ async function createTempFiles () {
 }
 
 function compile ({ ignore }) {
-  const babelPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', 'babel');
-  debug('Babel path =', babelPath);
-  return execa(babelPath, makeBabelArgs(ignore));
+  try {
+    // == attempt 1 ==
+    // dawson is installed globally (yarn/npm) or locally with yarn
+    // (yarn does not hoist .bin to the top)
+    const babelPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', 'babel');
+    debug('Babel attempt #1 with path =', babelPath);
+    return execa(babelPath, makeBabelArgs(ignore));
+  } catch (e) {
+    if (e.message.indexOf('ENOENT') === -1) {
+      throw e;
+    }
+    // == attempt 2 ==
+    // dawson is installed locally with npm
+    // (npm does hoist .bin to the top)
+    const babelPath = path.join(process.cwd(), 'node_modules', '.bin', 'babel');
+    debug('Babel attempt #2 with path =', babelPath);
+    return execa(babelPath, makeBabelArgs(ignore));
+  }
 }
 
 function install ({ skipChmod }) {
