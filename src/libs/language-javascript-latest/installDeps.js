3ca73a062e8ae67c43a4934892d57df4c036b489
Simone Lusenti <simone@plasticpanda.com>
2017-03-06 19:51:24 +0100

    adds logging

--- src/libs/language-javascript-latest/installDeps.js
+++ src/libs/language-javascript-latest/installDeps.js
@@ -1,8 +1,10 @@
 import execa from 'execa';
 import { oneLine } from 'common-tags';
-import os from 'os';
 
+import os from 'os';
 const IS_WINDOWS = os.platform() === 'win32';
+
+import { debug } from '../../logger';
 
 export default function install ({ skipChmod, rootDir }) {
   if (IS_WINDOWS) {
@@ -10,13 +12,16 @@ export default function install ({ skipChmod, rootDir }) {
     // Windows user will be required to download approx ~1.3GB of Docker images
     // also, when run on GNU/Linux, permissions on the .dawson-dist will be too restrictive
     // and only root can access
-    return execa.shell(oneLine`
+    const dockerCmd = oneLine`
       docker run
         -v ${rootDir}/.dawson-dist:/dawson-dist
         dawsonorg/install-deps:javascript-latest
-    `);
+    `;
+    debug('[windows-compat] installindg deps using docker:', dockerCmd);
+    return execa.shell(dockerCmd);
   }
   // if you modify the build commands below, be sure to update the Docker image too
+  debug('installing deps using system\'s npm');
   return execa.shell(
     oneLine`
       cd .dawson-dist &&
